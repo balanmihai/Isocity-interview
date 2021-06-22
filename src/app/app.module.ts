3a40cc04a6435c6ff17b857d81a54008af57350b
balaurl <mihai.balan.bala@gmail.com>
2021-06-22 03:47:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
+import { CenterComponent } from './component/center/center.component';
+import { TextComponent } from './component/text/text.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    CenterComponent,
+    TextComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
